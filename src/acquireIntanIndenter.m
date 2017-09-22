5b4bce264067c23fa279df2a4ad9d4a2d250a75e
Alan Emanuel <alan_emanuel@hms.harvard.edu>
2017-09-22 17:04:23 -0400

    added grid indentation

--- src/acquireIntanIndenter.m
+++ src/acquireIntanIndenter.m
@@ -42,13 +42,14 @@ switch protocol
     case 'forceIncreasingSteps'
         %% parameters
         stimulus = 'IndenterForceSteps';
-        sweepDuration = 20; % in s
-        interSweepInterval = 2; % in s
-        numSweeps = 20;
-        len = 4; % so that the maximum len will be ~ 1 mm above platform
-        intensities = [0.1, 0.2, 0.4, 0.8, 1.6];
+        sweepDuration = 10; % in s
+        interSweepInterval = 1; % in s
+        numSweeps = 1;
+        len_off = 0; % below platform for moving stage, best to be 0 so no sudden oscillation at beginning of stimulus
+        len_on = 5; % so that the maximum len will be ~ 1 mm above platform
+        intensities = [0.1, 0.2, 0.4, 0.8];
         stepFrequency = 0.5;
-        squareWaveT = 0:1/Fs:(.5*sweepDuration)-1/Fs;
+        squareWaveT = 0:1/Fs:(.8*sweepDuration)-1/Fs;
         squareWaveY = (square(2*pi*stepFrequency*squareWaveT,50)+1)/2;
         squareWaveY = squareWaveY';
         
@@ -68,12 +69,17 @@ switch protocol
         interSweepSamples = interSweepInterval * Fs;
         trigger = zeros(sweepDurationinSamples,1);
         trigger(2:1:end-1) = 1; % trigger determines length of intan recording, which will have buffer at beg and end
-        length = ones(sweepDurationinSamples,1) * len;
-        blankQuarter = zeros(sweepDurationinSamples*.25,1);
-        force = [blankQuarter; squareWaveY; blankQuarter];
+        length = ones(sweepDurationinSamples,1) * len_on;
+        blankTenth = zeros(sweepDurationinSamples*.1,1);
+        force = [blankTenth; squareWaveY; blankTenth];
         
         fullTrigger = repmat([trigger; zeros(interSweepSamples,1)],numSweeps,1);
-        fullLength = repmat([length; ones(interSweepSamples,1)*len],numSweeps,1);
+        fullLength = repmat([length; ones(interSweepSamples,1)*len_on],numSweeps,1);
+        
+        %ramping length up and down for first and last second in stimulus
+        fullLength(1:2e4) = len_off:(len_on-len_off)/2e4:len_on-1/2e4;
+        fullLength(end-2e4:end) = len_on:(len_off-len_on)/2e4:len_off-1/2e4;
+        
         fullForce = repmat([force; zeros(interSweepSamples,1)],numSweeps,1);
         
         %% Queue data
@@ -82,6 +88,70 @@ switch protocol
         
         [data, time] = s.startForeground();
         
+        
+    case 'forceLengthIncreasingSteps'
+        %% parameters
+        stimulus = 'IndenterForceLengthSteps';
+        sweepDuration = 10; % in s
+        sweepDurationinSamples = Fs * sweepDuration;
+        interSweepInterval = 3; % in s
+        numSweeps = 5;
+        len_off = -1; % so that len is below platform
+        len_on = 2; % so that the maximum len will be ~ 1 mm above platform
+        intensities = [0.1, 0.2, 0.4, 0.8];
+        stepFrequency = 0.5;
+        if size(intensities,2) ~= (.8*sweepDuration)*stepFrequency
+            error('# of force intensities incompatible with sweep duration')
+        end
+        squareWaveT = 0:1/Fs:(.8*sweepDuration)-1/Fs;
+        squareWaveY = (square(2*pi*stepFrequency*squareWaveT,50)+1)/2;
+        squareWaveY = squareWaveY';
+        
+        for i = 1:size(squareWaveY,1)
+            if squareWaveY(i) == 0
+                squareWaveY(i) = len_off;
+            elseif squareWaveY(i) == 1
+                squareWaveY(i) = len_on;
+            end
+        end
+        
+        % filter length signal to prevent oscillations
+        windowSize = 0.2*Fs;
+        b = (1/windowSize)*ones(1,windowSize);
+        a = 1;
+        
+        squareWaveY = filter(b, a, squareWaveY);
+        
+        forceWave = ones(sweepDurationinSamples*.8,1);
+        
+        forceWave(1:Fs*1.5) = forceWave(1:Fs*1.5) * intensities(1);
+        forceWave(Fs*1.5+1:Fs*3.5) = forceWave(Fs*1.5+1:Fs*3.5) * intensities(2);
+        forceWave(Fs*3.5+1:Fs*5.5) = forceWave(Fs*3.5+1:Fs*5.5) * intensities(3);
+        forceWave(Fs*5.5+1:end) = forceWave(Fs*5.5+1:end) * intensities(4);
+     
+        
+        clear squareWaveT
+        
+        %% build stimuli
+        interSweepSamples = interSweepInterval * Fs;
+        trigger = zeros(sweepDurationinSamples,1);
+        trigger(2:1:end-1) = 1; % trigger determines length of intan recording, which will have buffer at beg and end
+        
+        forceTenth = ones(sweepDurationinSamples*.1,1) * intensities(1);
+        force = [forceTenth; forceWave; forceTenth];
+            
+        lengthTenth = ones(sweepDurationinSamples*.1,1) * len_off;
+        length = [lengthTenth; squareWaveY; lengthTenth];
+
+        fullTrigger = repmat([trigger; zeros(interSweepSamples,1)],numSweeps,1);
+        fullLength = repmat([length; ones(interSweepSamples,1)*len_off],numSweeps,1);
+        fullForce = repmat([force; ones(interSweepSamples,1)*intensities(1)],numSweeps,1);
+        
+        %% Queue data
+        
+        s.queueOutputData(horzcat(fullTrigger, fullLength, fullForce))
+        
+        [data, time] = s.startForeground();        
     
     case 'forceSine'
         %% parameters
---
+++ src/indentOnGrid.m
@@ -0,0 +1,181 @@
+function [  ] = indentOnGrid( )
+%Apply indenter steps to points on a grid.
+%   Points  are ordered randomly.
+
+%% Set Parameters
+
+min_x = 4; % mm
+min_y = 4; % mm
+max_x = 14; % mm
+max_y = 14; % mm
+grid_spacing = 2; %mm
+move_velocity = 50; %mm/s
+num_repetitions = 2; % # of times repeating entire grid
+grid_x = repmat([min_x:grid_spacing:max_x],(max_y-min_y)/grid_spacing+1,1);
+
+grid_y = repmat([min_y:grid_spacing:max_y]',1,(max_x-min_x)/grid_spacing+1);
+
+grid_positions = [grid_x(:) grid_y(:)];
+
+rng(20170922) % set random seed for reproducibility
+
+grid_positions_rand = grid_positions(randperm(size(grid_positions,1)),:);
+
+grid_positions_actual = zeros(size(grid_positions_rand));
+
+%% Load PI MATLAB Driver GCS2
+%  (if not already loaded) should be already within saved path
+% addpath ( 'C:\Users\Public\PI\PI_MATLAB_Driver_GCS2', 'C:\Users\Public\PI\C-867\Samples\MATLAB' ); % If you are still using XP, please look at the manual for the right path to include.
+
+if ( ~exist ( 'Controller', 'var' ) || ~isa ( Controller, 'PI_GCS_Controller' ) )
+    Controller = PI_GCS_Controller ();
+end
+
+%% Start connection
+%(if not already connected)
+
+boolPIdeviceConnected = false; if ( exist ( 'PIdevice', 'var' ) ), if ( PIdevice.IsConnected ), boolPIdeviceConnected = true; end; end;
+if ( ~(boolPIdeviceConnected ) )
+
+    % Please choose the connection type you need.    
+    
+%    % RS232
+%    comPort = 1;          % Look at the Device Manager to get the rigth COM Port.
+%    baudRate = 115200;    % Look at the manual to get the rigth bau rate for your controller.
+%    PIdevice = Controller.ConnectRS232 ( comPort, baudRate );
+
+
+     % USB
+     controllerSerialNumber = '0117051110';    % Use "devicesUsb = Controller.EnumerateUSB('')" to get all PI controller connected to you PC.
+                                                                                     % Or look at the label of the case of your controller
+     PIdevice = Controller.ConnectUSB ( controllerSerialNumber );
+
+
+%     % TCP/IP
+%     ip = 'xxx.xxx.xx.xxx';  % Use "devicesTcpIp = Controller.EnumerateTCPIPDevices('')" to get all PI controller available on the network.
+%     port = 50000;           % Is 50000 for almost all PI controllers
+%     PIdevice = Controller.ConnectTCPIP ( ip, port ) ;
+
+end
+
+% Query controller identification string
+connectedControllerName = PIdevice.qIDN()
+
+% initialize PIdevice object for use in MATLAB
+PIdevice = PIdevice.InitializeController ();
+
+%% Show connected stages
+
+% query names of all controller axes
+availableAxes = PIdevice.qSAI_ALL
+
+
+% get Name of the stage connected to axis 1
+PIdevice.qCST ( '1' )
+
+
+% Show for all axes: which stage is connected to which axis
+for idx = 1 : length ( availableAxes )
+    % qCST gets the name of the 
+    stageName = PIdevice.qCST ( availableAxes { idx } );
+    disp ( [ 'Axis ', availableAxes{idx}, ': ', stageName ] );
+end
+
+% if the stages listed are wrong use one of the following solutions:
+% - PIMikroMove (recommended, but MS windows only)
+% - PITerminal  (use VST, qCST, CST and WPA command as described in the Manual)
+% - Use function "PI_ChangeConnectedStage"
+
+%% Startup Stage
+
+% This sections performs the startup commands valid for most but not all
+% PI devices. Depending on you sepcific device you will need to remove some
+% commands or add one of the following (list might be incomplete. Please
+% look at the manual if necessary):
+% - PIdevice.EAX ( axis, true );
+% - PIdevice.ATZ ( ... )
+% - You may need to interchange the order of FRF and SVO (this is the case for C-891)
+
+for axes = 1:size(availableAxes,2)
+    
+	axis = availableAxes{axes};
+
+    % switch servo on for axis
+    switchOn    = 1;
+    % switchOff   = 0;
+    PIdevice.SVO ( axis, switchOn );
+    PIdevice.VEL ( axis, move_velocity); % set speed of axes
+    % reference axis
+    PIdevice.FRF ( axis );  % find reference
+    bReferencing = 1;                           
+    disp ( ['Stage ' axis ' is referencing'] )
+    % wait for referencing to finish
+    while(0 ~= PIdevice.qFRF ( axis ) == 0 )                        
+        pause(0.1);           
+        fprintf('.');
+    end       
+    fprintf('\n');
+end
+
+
+%% Move Stages
+
+% determine the allowed travel range of the stage
+
+
+
+for gridLoc = 1:size(grid_positions_rand)
+    PIdevice.MOV ( availableAxes{1}, grid_positions_rand(gridLoc,1));
+    disp ( 'X axis stage is moving')
+    % wait for motion to stop
+    while(0 ~= PIdevice.IsMoving ( availableAxes{1} ) )
+        pause ( 0.05 );
+        fprintf('.');
+    end
+    fprintf('\n');
+    
+    PIdevice.MOV ( availableAxes{2}, grid_positions_rand(gridLoc,2));
+    disp ( 'Y axis stage is moving')
+    % wait for motion to stop
+    while(0 ~= PIdevice.IsMoving ( availableAxes{2} ) )
+        pause ( 0.05 );
+        fprintf('.');
+    end
+    fprintf('\n');
+    pause(1) %% rest of code will go here
+    grid_positions_actual(gridLoc,1) = PIdevice.qPOS(availableAxes{1});
+    grid_positions_actual(gridLoc,2) = PIdevice.qPOS(availableAxes{2});
+    
+    disp('Stimulating')
+    acquireIntanIndenter('forceIncreasingSteps')
+    
+end
+
+%% If you want to close the connection
+PIdevice.CloseConnection ();
+
+%% If you want to unload the dll and destroy the class object
+Controller.Destroy ();
+clear Controller;
+clear PIdevice;
+
+
+%% Save structure with stimulus information
+s1.stimulus = 'GridIndent';
+s1.min_x = min_x;
+s1.max_x = max_x;
+s1.min_y = min_y;
+s1.max_y = max_y;
+s1.grid_spacing = grid_spacing;
+s1.grid_positions = grid_positions;
+s1.grid_positions_rand = grid_positions_rand;
+s1.grid_positions_actual = grid_positions_actual;
+
+path = 'E:\DATA\';
+fullpath = strcat(path,s1.stimulus,'_', datestr(now,'yymmdd HHMM SS'), '.mat');
+fprintf('saved as %s\n', fullpath)
+save(fullpath, '-struct', 's1');
+
+%positonReached = PIdevice.qPOS(axis)
+end
+
